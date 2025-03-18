178288299d40de151d693594d382edc4aba81e8d
APauquin <alexandrep123@yahoo.com>
2025-03-18 11:45:53 +0100

    Github pages debug attempt 2

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-import glsl from 'vite-plugin-glsl' // Import GLSL Plugin
+import glsl from 'vite-plugin-glsl'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    tailwindcss(),
-    glsl() // Add GLSL Plugin
+    glsl() 
   ],
 })
