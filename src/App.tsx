cd56be07357c54d039a7eb0a41a8f0b2d4642a45
APauquin <alexandrep123@yahoo.com>
2025-03-18 12:23:43 +0100

    Github pages debug attempt 5

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import MainLayout from "./components/layout/MainLayout";
 import Home from "./pages/Home";
 import NotFound from "./pages/NotFound";
